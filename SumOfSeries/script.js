e78cb5d91d518c4d43d2716b92bffda829b0ab93
Omendra Rajput <omendrarajput2@gmail.com>
2022-02-13 20:46:55 +0530

    seriesSum

---
+++ SumOfSeries/script.js
@@ -0,0 +1,14 @@
+//series sum
+
+function SeriesSum(n) {
+    var sum = 0;
+    for(var i = 0; i < n; i++) {
+      sum += 1 / (3 * i + 1);
+    }
+    return sum.toFixed(2);
+  }
+    
+    
+    
+    
+  
